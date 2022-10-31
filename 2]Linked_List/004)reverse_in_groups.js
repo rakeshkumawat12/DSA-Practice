b4f4bd5a5f42c39467e72560c1fc867c4306c530
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-10-31 11:24:03 +0530

    Added linkedlist

--- 2]Linked_List/004)reverse_in_groups.js
+++ 2]Linked_List/004)reverse_in_groups.js
@@ -7,27 +7,34 @@ class Node {
     }
 }
 function reverse(head, k) {
-    if (head == null) {
-        console.log("NULL");
+    let cur, prev, next, start1, start2;
+    let flag = 0, m = 0;
+    cur = head;
+    prev = null;
+    start1 = cur;
+    while (cur != null) {
+        for (let i = 0; i < k; i++) {
+            if (cur == null)
+                break;
+
+            next = cur.next;
+            cur.next = prev
+            prev = cur;
+            cur = next;
+        }
+        if (m != 0) {
+            start1.next = prev;
+            start1 = start2
+        }
+        start2 = cur
+        m++;
+        if (flag == 0) {
+            head = prev;
+            flag = 1;
+        }
+        prev = null
     }
-    var current = head;
-    var next = null;
-    var prev = null;
-
-    var count = 0;
-
-    while (count < k && current != null) {
-        next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
-        count++;
-    }
-
-    if (next != null)
-        head.next = reverse(next, k);
-
-    console.log(prev);
+    console.log(head)
 }
 function push(data) {
     newNode = new Node(data);
@@ -36,15 +43,12 @@ function push(data) {
 }
 
 
-push(9);
-push(8);
-push(7);
-push(6);
 push(5);
 push(4);
 push(3);
 push(2);
 push(1);
+
 
 reverse(head, 3);
 
