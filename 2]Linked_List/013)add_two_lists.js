cc7cd19cdf876209ccd181f5c611dcf2e70b71fd
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-11-01 12:12:15 +0530

    Added linkedlist

--- 2]Linked_List/013)add_two_lists.js
+++ 2]Linked_List/013)add_two_lists.js
@@ -1,55 +1,64 @@
-reverse(head) 
-    {
-        let prev = null;
-        let current = head;
-        let next;
-        
-        while (current !== null) 
-        { 
-            next = current.next;   
-            current.next = prev;    
-            prev = current;           
-            current = next;           
-        }
-        
-        return prev; 
+class Node {
+    constructor(data) {
+        this.value = data;
+        this.next = null;
     }
-    //Function to add two numbers represented by linked list.
-    addTwoLists(first, second)
-    {
-        //reversing both lists to simplify addition.
-        first = this.reverse(first);             
-        second = this.reverse(second);
-        
-        let sum = null;
-        let carry = 0;
-        
-        //using a loop till both lists and carry gets exhausted.
-        while( first !== null || second !== null || carry !== 0 )
-        {
-            //using a variable to store sum of two digits along with carry.
-            let newVal = carry;
-            
-            //if nodes are left in any list, we add their data in newVal.
-            if(first) newVal += first.data;
-            if(second) newVal += second.data;
-            
-            //updating carry.
-            carry = Math.floor(newVal/10); 
-            //using modulus to store only a single digit at that place.
-            newVal = newVal%10;    
-            
-            //creating new node which gets connected with other nodes that 
-            //we get after calculating sum of respective digits.
-            let newNode = new Node(newVal);
-            //storing the previously made nodes in the link part of new node.
-            newNode.next = sum;   
-            //making the new node as the first node of all previously made node.
-            sum = newNode;
-            
-            //moving ahead in both lists.
-            if(first) first = first.next;     
-            if(second) second = second.next;
-        }
-        return sum;
+}
+
+function reverseList(head) {
+    var prev = null;
+    var current = head;
+    var next = null;
+
+    while (current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
     }
+    return prev
+}
+
+function addTwoList(head1, head2) {
+    head1 = reverseList(head1);
+    head2 = reverseList(head2);
+    let head = null;
+    let carry = 0, sum;
+    while (head1 || head2) {
+        if (head1 && head2)
+            sum = head1.value + head2.value + carry;
+        else if (head1)
+            sum = head1.value + carry;
+        else
+            sum = head2.value + carry;
+        if (sum > 9) {
+            carry = sum / 10;
+            sum = sum % 10;
+        }
+        else
+            carry = 0;
+        let newNode = new Node(sum)
+        newNode.next = head;
+        head = newNode;
+        if (head1)
+            head1 = head1.next;
+        if (head2)
+            head2 = head2.next;
+    }
+    if (carry != 0) {
+        let newNode = new Node(carry)
+        newNode.next = head;
+        head = newNode;
+    }
+    console.log(head)
+}
+
+head1 = new Node(5);
+head1.next = new Node(4);
+
+
+head2 = new Node(5);
+head2.next = new Node(4);
+head2.next.next = new Node(3);
+
+addTwoList(head1, head2)
