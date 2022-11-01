929bba7810a4e9dab17a97436fd9c7ab974fbe29
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-11-01 11:23:23 +0530

    Added linkedlist

--- 2]Linked_List/012)pair_wise_swap.js
+++ 2]Linked_List/012)pair_wise_swap.js
@@ -1,24 +1,43 @@
-pairWiseSwap(node)
-{
-    let a = head, b = null, c = null, prev = null;
-
-    while (a != null && a.next != null) {
-        b = a.next;     // b is second node
-        c = b.next;     // c is third node (c might be null)
-        // current order : prev - a - b - c
-        if (a == head)
-            // b will be new head
-            head = b;
-        else
-            // else, linking second node i.e. b, to a's ancestor
-            prev.next = b;
-
-        b.next = a;     // a should now come after b
-        a.next = c;     // connecting a to list ahead
-
-        // now we have to update a and prev for next pair
-        prev = a;
-        a = c;
+var head;
+class Node{
+    constructor(data){
+        this.value = data;
+        this.next = null;
     }
-    return head;
 }
+
+function pairWiseSwap(head){
+  let temp = head, first = head, second = head;
+  let flag = 0;
+  while(second.next != null){
+    first = second.next;
+    second.next = first.next;
+    first.next = second;
+    if(flag == 0)
+      head = first
+    if(flag != 0){
+      temp.next = first;
+      temp = second;
+    }
+    second = second.next;
+    first = second;
+
+    if (second == null)
+      break;
+    flag = 1;
+  }
+  console.log(head);
+}
+
+function push(data){
+  newNode = new Node(data);
+  newNode.next = head;
+  head = newNode;
+} 
+
+push(5)
+push(4)
+push(3)
+push(2)
+push(1)
+pairWiseSwap(head)
