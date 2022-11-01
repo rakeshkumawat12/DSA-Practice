1142e0d8f27fa48c584f00aadc3e4ca2332e73f6
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-11-01 10:43:38 +0530

    Added linkedlist

--- 2]Linked_List/011)sorted_merged.js
+++ 2]Linked_List/011)sorted_merged.js
@@ -1,39 +1,61 @@
-sortedMerge(head1, head2)
-{
-    //creating a dummy first node to hang the result on.
-    let dummy = new Node(0);
 
-    //a pointer, tail is used to store the resultant list after merging.
-    let tail = dummy;
-
-    while (1) {
-        //if either list runs out, we store all the nodes 
-        //of other list in link part of tail node.
-        if (head1 === null) {
-            tail.next = head2;
-            break;
-        }
-        else if (head2 === null) {
-            tail.next = head1;
-            break;
-        }
-        //comparing the data of the two lists and storing the node
-        //with smaller data in link part of the tail node.
-        if (head1.data <= head2.data) {
-            tail.next = head1;
-            //if data in first list is smaller, we move to 
-            //the next node in it.
-            head1 = head1.next;
-        }
-        else {
-            tail.next = head2;
-            //if data in second list is smaller, we move to 
-            //the next node in it.
-            head2 = head2.next;
-        }
-        //moving to the next node.
-        tail = tail.next;
+class Node{
+    constructor(data){
+        this.value = data;
+        this.next = null;
     }
-    //returning the merged list attached to dummy.
-    return dummy.next;
 }
+
+function sortedMerged(head1, head2){
+  let temp, head;
+  let flag = 0;
+  while(head1 != null && head2 != null){
+    if (head1.value < head2.value){
+      if(flag == 0){
+        head = head1;
+        temp = head1;
+        flag = 1;
+      }
+      else{
+        temp.next = head1;
+        temp = temp.next
+      }
+      head1 = head1.next;
+    }
+    else{
+      if(flag == 0){
+        head = head2;
+        temp = head2;
+        flag = 1;
+      }
+      else{
+        temp.next = head2;
+        temp = temp.next
+      }
+      head2 = head2.next;
+    }
+  }
+  while(head1 != null){
+    temp.next = head1;
+    temp = temp.next;
+    head1 = head1.next;
+  }
+  while(head2 != null){
+    temp.next = head2;
+    temp = temp.next;
+    head2 = head2.next;
+  }
+  console.log(head)
+}
+
+head1 = new Node(3);
+head1.next = new Node(6);
+head1.next.next = new Node(9);
+head1.next.next.next = new Node(15);
+head1.next.next.next.next = new Node(30);
+
+head2 = new Node(5);
+head2.next = new Node(15);
+head2.next.next = new Node(30);
+
+sortedMerged(head1, head2)
