237506cbc79c4b77d7a04e3e069b0b366b394f30
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-11-01 15:32:55 +0530

    Added linkedlist

--- 2]Linked_List/017)sort.js
+++ 2]Linked_List/017)sort.js
@@ -1,7 +1,7 @@
 var head;
 class Node {
     constructor(data) {
-        this.data = data;
+        this.value = data;
         this.next = null;
     }
 }
@@ -10,27 +10,27 @@ function segregate(head) {
     let temp = head;
     let count0 = 0, count1 = 0, count2 = 0, flag = 0;
     while (temp != null) {
-        if (temp.data == 0)
+        if (temp.value == 0)
             count0++;
-        else if (temp.data == 1)
+        else if (temp.value == 1)
             count1++;
-        else if (temp.data == 2)
+        else if (temp.value == 2)
             count2++;
         temp = temp.next
     }
     temp = head;
     while (count0 > 0) {
-        temp.data = 0;
+        temp.value = 0;
         temp = temp.next;
         count0--;
     }
     while (count1 > 0) {
-        temp.data = 1;
+        temp.value = 1;
         temp = temp.next;
         count1--;
     }
     while (count2 > 0) {
-        temp.data = 2;
+        temp.value = 2;
         temp = temp.next;
         count2--;
     }
