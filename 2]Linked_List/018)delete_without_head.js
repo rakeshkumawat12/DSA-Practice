20e13b44354bf39e95f19a16d9a479bf35ca158c
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-11-28 20:58:25 +0530

    Improved

--- 2]Linked_List/018)delete_without_head.js
+++
@@ -1,9 +0,0 @@
-deleteNode(del)
-    {
-        while(del.next.next != null){
-            del.data = del.next.data;
-            del = del.next;
-        }
-        del.data = del.next.data;
-        del.next = null;
-    }
